eefd3ae58da2cc5e125df782e8f5fd8d63cf9c46
Matthias Bagehorn <29986457+MatthiGamer@users.noreply.github.com>
2023-07-24 16:03:45 +0200

    Fixed Path

--- Start.bat
+++ Start.bat
@@ -1,3 +1,3 @@
 @echo off
-start "%~dp0"WebSocketServer\bin\Release\net6.0\WebSocketServer.exe "%~dp0"\WebSocketServerProject\WebSocketServer\bin\Release\net6.0\WebSocketServer.exe
+start "%~dp0"\WebSocketServer\bin\Release\net6.0\WebSocketServer.exe "%~dp0"\WebSocketServer\bin\Release\net6.0\WebSocketServer.exe
 start cmd /c "cd "%~dp0"\websocketclient & serve -s build"
